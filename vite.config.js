d1ec04334cf9b47a0f22c4e4973f93f3c5e5a4f5
EMTnoon <2683102724@qq.com>
2022-08-14 10:57:30 +0800

    模块打包

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/dist/',
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+              if (id.includes('node_modules')) {              
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+        }
+    }
+  },
   plugins: [
     vue(),
     AutoImport({ 
